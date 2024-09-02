2bff3b2842122b5596464b7207759406f99125cf
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2024-09-02 12:07:29 +0600

    Product Sorting working fine

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -11,6 +11,7 @@ const Collection = () => {
     const [filterProducts, setFilterProducts] = useState([]);
     const [category, setCategory] = useState([]);
     const [subCategory, setSubCategory] = useState([]);
+    const [sortType, setSortType] = useState('relavent');
 
 
     const toggleCategory = (e) => {
@@ -45,14 +46,32 @@ const Collection = () => {
         setFilterProducts(productsCopy)
     }
 
+    const sortProduct = () => {
 
-    useEffect(() => {
-        setFilterProducts(products);
-    }, [])
+        let fpCopy = filterProducts.slice();
+
+        switch (sortType) {
+            case 'low-high':
+                setFilterProducts(fpCopy.sort((a, b) => (a.price - b.price)));
+                break;
+            case 'high-low':
+                setFilterProducts(fpCopy.sort((a, b) => (b.price - a.price)));
+                break;
+            default:
+                applyFilter();
+                break;
+        }
+
+    }
+
 
     useEffect(() => {
         applyFilter()
     }, [category, subCategory])
+
+    useEffect(() => {
+        sortProduct();
+    }, [sortType])
 
 
     return (
@@ -97,7 +116,7 @@ const Collection = () => {
                 <div className="flex justify-between text-base sm:text-2xl mb-4">
                     <Title text1={"All "} text2={" COLLECTION"} />
                     {/* Product Sort */}
-                    <select className="border-2 border-gray-200 text-sm px-2">
+                    <select onChange={(e) => setSortType(e.target.value)} className="border-2 border-gray-200 text-sm px-2">
                         <option value="relevant">Sort by: Relevant</option>
                         <option value="low-high">Sort by: Low to High</option>
                         <option value="high-low">Sort by: High to Low</option>

b83600d0684ea6be3e674d6e22891ff3db015d5e
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2024-09-05 12:09:21 +0600

    Cart Page Ready Working Calulate product Total amount & more

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types"; // Import PropTypes for type checking
 import { products } from '../assets/frontend_assets/assets.js'; // Import product data
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 // Create a new context for the shop
 export const ShopContext = createContext();
@@ -15,6 +16,7 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState(''); // Stores the search query
     const [showSearch, setShowSearch] = useState(false); // Controls the visibility of the search bar
     const [cartItems, setCartItems] = useState({});
+    const navigate = useNavigate();
 
 
     const addToCart = async (itemId, size) => {
@@ -55,6 +57,30 @@ const ShopContextProvider = (props) => {
         return totalCount;
     }
 
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId][size] = quantity;
+
+        setCartItems(cartData);
+    }
+
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items);
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalAmount += itemInfo.price * cartItems[items][item];
+                    }
+                } catch (error) {
+
+                }
+            }
+        }
+        return totalAmount;
+    }
+
     // Combine all the state and static data into a single value object
     const value = {
         products, // Product list
@@ -62,7 +88,7 @@ const ShopContextProvider = (props) => {
         delivery_fee, // Delivery fee
         search, setSearch, // Search state and setter
         showSearch, setShowSearch,
-        cartItems, addToCart, getCartCount
+        cartItems, addToCart, getCartCount, updateQuantity, getCartAmount, navigate
         // Search bar visibility state and setter
     };
 

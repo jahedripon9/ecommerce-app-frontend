b591bceffb5197385a0cfdfe2cc6c4be4490c872
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2024-09-07 09:57:06 +0600

    Place order Page Ready

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -7,7 +7,7 @@ const Title = ({ text1, text2 }) => {
                 {text1}
                 <span className="text-gray-700 font-medium">{text2}</span>
             </p>
-            <p className="w-8 sm:w-12 h-[1px] sm:h-[2px] bg-gray-700"></p>
+            <p className="w-8 sm:w-12 h-[2px] sm:h-[2px] bg-gray-700"></p>
         </div>
     );
 };
